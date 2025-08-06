ba1c23ff08851a22765f89721aa3061d2b10e345
unknown <kartikeemehta4002@gmail.com>
2025-08-06 22:33:48 +0530

    routes updated

--- routes/teamRoutes.js
+++ routes/teamRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const teamController = require("../controllers/teamController");
 const teamAuthMiddleware = require("../middleware/teamAuthMiddleware");
-const employeeMiddleware = require("../middleware/employeeMiddleware");
+const employeeMiddleware = require("../middleware/EmployeeMiddleware");
 const authMiddleware = require("../middleware/authMiddleware");
 
 router.post("/createTeam", authMiddleware, teamController.createTeam);
@@ -11,7 +11,11 @@ router.put("/updateTeam", authMiddleware, teamController.updateTeam);
 router.get("/team-leads", authMiddleware, teamController.getAllTeamLeads);
 router.get("/team-members", authMiddleware, teamController.getAllTeamMembers);
 router.get("/all-teams", authMiddleware, teamController.getAllTeams);
-router.get("/:teamName/members", authMiddleware, teamController.getTeamMembersByTeamName);
+router.get(
+  "/:teamName/members",
+  authMiddleware,
+  teamController.getTeamMembersByTeamName
+);
 
 // no delete one
 
